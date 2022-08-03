afcd1fb3c3a4e58e26145e40f98a17a972b09483
zainnizami444 <98643436+zainnizami444@users.noreply.github.com>
2022-08-03 19:45:37 +0500

    drawer

---
+++ Drawer/app/src/main/java/com/example/drawer/delete/delete.java
@@ -0,0 +1,34 @@
+package com.example.drawer.delete;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.drawer.R;
+import com.example.drawer.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class delete extends Fragment {
+    ArrayList<User> data;
+    DatabaseReference db;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_delete,container,false);
+
+        return root;
+    }
+
+}
---
+++ Drawer/app/src/main/java/com/example/drawer/ui/slideshow/SlideshowFragment.java
@@ -0,0 +1,34 @@
+package com.example.drawer.ui.slideshow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.drawer.Edit;
+import com.example.drawer.R;
+import com.example.drawer.User;
+import com.example.drawer.databinding.FragmentSlideshowBinding;
+import com.example.drawer.ui.home.HomeFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SlideshowFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+
+    return  root;
+    }
+}
